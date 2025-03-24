69beae8f35e25eb0e6d5f47b067a095ec4228b74
Pavel <pasha.tomiloff@gmail.com>
2025-03-24 16:15:10 +0300

    burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 // Main JavaScript file
 "use strict"
+const wrapper = document.querySelector('.icon-burger');
+wrapper.addEventListener('click', myFunction);
+
+function myFunction() {
+  let element = document.querySelector('.menu');
+  element.classList.toggle("menu__open");
+  wrapper.classList.toggle("change");
+}
